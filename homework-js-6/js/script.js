5434d73a06c6d66c0dd41d17e0331c54f5f50cde
iraboldyrieva <156907500+iraboldyrieva@users.noreply.github.com>
2024-03-14 12:07:59 +0200

    add JS homework 6

---
+++ homework-js-6/js/script.js
@@ -0,0 +1,33 @@
+// Створити телефонну книгу
+// створити початковий клас Abonent, де зберігатимуться ім*я і номер
+// створити set який прийматиме телефон і номер
+// створити get який виводитиме данні про абонента
+// створити три різних юзери
+// вивести данні
+
+// class Abonent {
+//   constructor(name, tel) {
+//     this._name = name;
+//     this._tel = tel;
+//   }
+
+//   set name(newName) {
+//     this._name = newName;
+//   }
+
+//   set tel(newTel) {
+//     this._tel = newTel;
+//   }
+
+//   get info() {
+//     return `Abonent: ${this._name}, tel: ${this._tel}`;
+//   }
+// }
+
+// const abonent = new Abonent("Oleg", "123456789");
+// const abonent2 = new Abonent("Marry", "0957654321");
+// const abonent3 = new Abonent("Andrew", "0939876543");
+
+// console.log(abonent.info);
+// console.log(abonent2.info);
+// console.log(abonent3.info);
